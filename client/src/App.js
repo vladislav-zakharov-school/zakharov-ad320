2f6ab50067900d0da9a5ff2ba4d6d13d5b421797
Vladislav Zakharov <vlad@Vladislavs-MacBook-Pro.local>
2022-02-07 20:54:11 -0800

    basic layout 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
 import Topbar from "./components/Topbar/Topbar";
-import Page from "./components/Page/Page";
+import Card from "./components/Card/Card"
+import Button from "./components/Button/Button"
+
 
 function App() {
   return (
     <React.Fragment>
       <Topbar />
-      <Page />
+      <div className="page">
+        <div className="sidebar">
+          <p>hello there</p>
+        </div>
+        <div className="content">
+          <Card />
+          <div className="navigation">
+            <Button />
+            <Button />
+            <Button />
+          </div>
+        </div>
+        <div className="sidebar">
+          <p>hello there</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
